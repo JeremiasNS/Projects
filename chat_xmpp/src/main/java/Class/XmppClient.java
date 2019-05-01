3763b028e648389cdf3ee7fda141cd301262e80e
Jeremias Neres <jeremiasneres23@gmmail.com>
2019-04-30 21:41:53 -0300

    Projeto xmpp inicial

--- chat_xmpp/src/main/java/Class/XmppClient.java
+++ chat_xmpp/src/main/java/Class/XmppClient.java
@@ -1,12 +1,31 @@
 package Class;
 
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jxmpp.stringprep.XmppStringprepException;
+
 public class XmppClient {
-    
-    public XmppClient(){
+
+    private String username;
+    private String password;
+    private XMPPTCPConnectionConfiguration.Builder builder;
+    private XMPPTCPConnection connection;
+
+    public XmppClient() throws XmppStringprepException {
+        builder = XMPPTCPConnectionConfiguration.builder();
+        builder
+                .setXmppDomain("xabber.org")
+                .setHost("xabber.org")
+                .setUsernameAndPassword(username, password);
     }
-    
+
     public static void main(String[] args) {
     }
-    
-}
 
+    public boolean connect() {
+    }
+
+    public boolean login() {
+    }
+
+}
