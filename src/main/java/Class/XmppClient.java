86a8a488c66a0252b78f6da6bc1368b5137babbd
Jeremias Neres <jeremiasneres23@gmail.com>
2019-05-07 19:35:23 -0300

    Adicionado mais conteudo

---
+++ src/main/java/Class/XmppClient.java
@@ -0,0 +1,60 @@
+package Class;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jivesoftware.smack.chat2.ChatManager;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jxmpp.stringprep.XmppStringprepException;
+
+public class XmppClient {
+
+    private String username;
+    private String password;
+    private XMPPTCPConnectionConfiguration.Builder builder;
+    private XMPPTCPConnection connection;
+    private ChatManager chatManager;
+
+    public XmppClient() throws XmppStringprepException {
+        builder = XMPPTCPConnectionConfiguration.builder();
+        builder
+                .setXmppDomain("xabber.org")
+                .setHost("xabber.org")
+                .setUsernameAndPassword("jeremias.net", "jerenet@tcp")
+                .setResource("desktop-app");
+        
+        connection = new XMPPTCPConnection(builder.build());
+    }
+
+    public static void main(String[] args) {
+        XmppClient app = new XmppClient();
+        app.connect();
+    }
+
+    public boolean connect() {
+        try {
+            connection.connect();
+           // chatManager = Chat
+        } catch (SmackException | IOException | XMPPException | InterruptedException e) {
+            throw new RuntimeException(
+            "Não foi possivel conectar ao servidor", e);
+            
+        }
+        
+        chatManager = ChatManager.getInstanceFor(connection);
+        
+        try {
+            connection.login();
+        } catch (SmackException | IOException | XMPPException | InterruptedException e) {
+            throw new RuntimeException(
+            "Não foi possivel loga no servidor", e);
+            
+        }
+        
+        return true;
+    }
+
+}
